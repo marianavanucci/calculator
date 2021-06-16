5090dbd5c7059ee83c97ae0ec763949d27bc8a08
MarianaVanucci <marianavanucciandrade@gmail.com>
2021-06-16 15:43:28 -0300

    constructor numberButtons updateDisplay

--- scripts.js
+++ scripts.js
@@ -1,7 +1,52 @@
 const numberButtons = document.querySelectorAll('[data-number]')
+Calculator {
+
+constructor(previousOperandTextElement, currentOperandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+}
+// cria os objetos previousOperandTextElement e currentOperandTextElement
+
+clear(){
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+}
+
+delete(){
+
+}
+
+appendNumber(){
+
+}
+chooseOperation(){
+
+}
+
+compute(){
+
+}
+
+updateDisplay(){
+    this.currentOperandTextElement.innerText = this.currentOperand
+}
+
+}
+
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach( button => {
+button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+})
+})
