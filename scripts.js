6839e108591daf5e76ea6e38615cf81a3cb4dd0c
MarianaVanucci <marianavanucciandrade@gmail.com>
2021-06-17 23:56:17 -0300

    equalsButton bug fix

--- scripts.js
+++ scripts.js
@@ -72,7 +72,7 @@ updateDisplay(){
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalButton = document.querySelector('[data-equals]')
+const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
